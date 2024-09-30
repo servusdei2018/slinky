b324bf9e2196f550f9c26f58718f61fe786d0514
Nathanael Bracy <nate@bracy.dev>
2024-09-30 16:58:13 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# slinky
+
+**slinky** is a command-line browser designed to navigate through linked plain text files using anchors.
+
+## Features
+- Navigation using anchors.
+- History tracking with `back` command.
+- Customizable line formatting.
+- Handles line breaks and paragraphs.
+
+## Installation
+
+```sh
+TODO
+```
+
+## Design
+
+For a detailed overview of the project's goals, features, and architecture, please refer to the [Design Document](design.pdf).
+
+## License
+
+This project is licensed under the MIT License. Please refer to the `LICENSE` file for details.
---
+++ src/slinky.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+#include "FileHandler/FileHandler.hpp"
+#include "AnchorProcessor/AnchorProcessor.hpp"
+#include "Navigator/Navigator.hpp"
+#include "TextFormatter/TextFormatter.hpp"
+
+void displayHelp() {
+    std::cout << "Commands:\n";
+    std::cout << "  go <n>     - Navigate to the document associated with anchor number n\n";
+    std::cout << "  back       - Go back to the previous document\n";
+    std::cout << "  help       - Display this help message\n";
+    std::cout << "  exit       - Exit the browser\n";
+}
+
+int main() {
+    std::string currentFile;
+
+    std::cout << "Welcome to the Slinky Web Browser!\n";
+    displayHelp();
+    
+    std::cout << "Please enter the initial file to open: ";
+    std::getline(std::cin, currentFile);
+
+    std::string content;
+    while (currentFile != "exit") {
+        std::string command;
+        std::cout << "> ";
+        std::getline(std::cin, command);
+
+        if (command == "exit") {
+            break;
+        } else if (command == "help") {
+            displayHelp();
+        } else {
+            std::cout << "Unknown command. Type 'help' for a list of commands.\n";
+        }
+    }
+
+    std::cout << "Exiting the Slinky Web Browser. Goodbye!\n";
+    return 0;
+}
